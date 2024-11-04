e956d61145c0d3aa9a33875978997e141ddd0027
phimwa958 <pim2544varee@gmail.com>
2024-11-05 03:37:29 +0700

    Update perceptron.py
    Added train() and calculate_percent_error() functions.

--- perceptron.py
+++ perceptron.py
@@ -1,121 +1,107 @@
-# Vraj, Patel
-# 2019-09-22
-
 import numpy as np
-import itertools
 
 class Perceptron(object):
-    def __init__(self, input_dimensions=2,number_of_classes=4,seed=None):
+    def __init__(self, input_dimensions=2, number_of_classes=4, seed=None):
         """
         Initialize Perceptron model
-        :param input_dimensions: The number of features of the input data, for example (height, weight) would be two features.
-        :param number_of_classes: The number of classes.
-        :param seed: Random number generator seed.
+        :param input_dimensions: The number of features of the input data
+        :param number_of_classes: The number of classes
+        :param seed: Random number generator seed
         """
-        if seed != None:
+        if seed is not None:
             np.random.seed(seed)
         self.input_dimensions = input_dimensions
-        self.number_of_classes=number_of_classes
+        self.number_of_classes = number_of_classes
         self._initialize_weights()
+
     def _initialize_weights(self):
         """
-        Initialize the weights, initalize using random numbers.
-        Note that number of neurons in the model is equal to the number of classes
+        Initialize the weights randomly
         """
-        self.weights = np.random.randn(self.number_of_classes,self.input_dimensions+1)
-        #print(self.weights)
-        #print("No of classes:",number_of_classes)
-        #raise Warning("You must implement _initialize_weights! This function should initialize (or re-initialize) your model weights. Bias should be included in the weights")
+        self.weights = np.random.randn(self.number_of_classes, self.input_dimensions + 1)
 
     def initialize_all_weights_to_zeros(self):
         """
-        Initialize the weights, initalize using random numbers.
+        Initialize all weights to zeros
         """
-        self.weights = np.zeros((self.number_of_classes,self.input_dimensions+1))
-        #print(self.weights)
-        #raise Warning("You must implement this function! This function should initialize (or re-initialize) your model weights to zeros. Bias should be included in the weights")
+        self.weights = np.zeros((self.number_of_classes, self.input_dimensions + 1))
 
     def predict(self, X):
         """
         Make a prediction on an array of inputs
-        :param X: Array of input [input_dimensions,n_samples]. Note that the input X does not include a row of ones
-        as the first row.
-        :return: Array of model outputs [number_of_classes ,n_samples]
+        :param X: Array of input [input_dimensions, n_samples]
+        :return: Array of model outputs [number_of_classes, n_samples]
         """
-        """
-        row = []
-        for i in range (0,self.weights.shape[0]):
-            activation = []
-            for j in range (0,X.shape[1]):
-                summation = np.dot(X[:,j],((self.weights[i,:self.weights.shape[1]-1].T))) + self.weights[i][self.weights.shape[1]-1]
-                if summation > 0:
-                    activation.append(1)
-                else:
-                    activation.append(0)            
-            row.append(activation)
-        return(row)
-        #raise Warning("You must implement predict. This function should make a prediction on a matrix of inputs")
-        """
-        temp = np.ones((1,X.shape[1]))
-        X = np.r_[temp, X]
-        activation = np.dot(self.weights,X)
-        activation[activation < 0] = 0
+        temp = np.ones((1, X.shape[1]))  # Add a row of 1s for the bias term
+        X = np.r_[temp, X]  # Concatenate bias with input
+        activation = np.dot(self.weights, X)
+        activation[activation < 0] = 0  # Binary step function
         activation[activation > 0] = 1
-        return(activation)
+        return activation
 
     def print_weights(self):
         """
-        This function prints the weight matrix (Bias is included in the weight matrix).
+        Print the weight matrix (Bias included)
         """
-        return(self.weights)
-        #raise Warning("You must implement print_weights")
+        return self.weights
 
     def train(self, X, Y, num_epochs=10, alpha=0.001):
         """
-        Given a batch of data, and the necessary hyperparameters,
-        this function adjusts the self.weights using Perceptron learning rule.
-        Training should be repeted num_epochs time.
-        :param X: Array of input [input_dimensions,n_samples]
-        :param y: Array of desired (target) outputs [number_of_classes ,n_samples]
-        :param num_epochs: Number of times training should be repeated over all input data
+        Adjust weights using Perceptron learning rule
+        :param X: Array of input [input_dimensions, n_samples]
+        :param Y: Array of desired (target) outputs [number_of_classes, n_samples]
+        :param num_epochs: Number of training epochs
         :param alpha: Learning rate
-        :return: None
         """
+        # Add row of 1s to X for the bias term
+        temp = np.ones((1, X.shape[1]))
+        X = np.r_[temp, X]
+        
+        for epoch in range(num_epochs):
+            predictions = self.predict(X[1:, :])  # Predict based on current weights
+            errors = Y - predictions  # Calculate error
+            
+            # Update weights based on the error and learning rate
+            for i in range(X.shape[1]):
+                self.weights += alpha * np.outer(errors[:, i], X[:, i])
 
-        raise Warning("You must implement train")
-
-    def calculate_percent_error(self,X, Y):
+    def calculate_percent_error(self, X, Y):
         """
-        Given a batch of data this function calculates percent error.
-        For each input sample, if the output is not hte same as the desired output, Y,
-        then it is considered one error. Percent error is number_of_errors/ number_of_samples.
-        :param X: Array of input [input_dimensions,n_samples]
-        :param y: Array of desired (target) outputs [number_of_classes ,n_samples]
-        :return percent_error
+        Calculate percent error of predictions
+        :param X: Array of input [input_dimensions, n_samples]
+        :param Y: Array of desired (target) outputs [number_of_classes, n_samples]
+        :return: Percent error
         """
-        raise Warning("You must implement calculate_percent_error")
+        predictions = self.predict(X)
+        number_of_errors = np.sum(np.any(predictions != Y, axis=0))
+        percent_error = (number_of_errors / X.shape[1]) * 100
+        return percent_error
 
+# Main program
 if __name__ == "__main__":
-    """
-    This main program is a sample of how to run your program.
-    You may modify this main program as you desire.
-    """
-
     input_dimensions = 2
     number_of_classes = 2
 
     model = Perceptron(input_dimensions=input_dimensions, number_of_classes=number_of_classes, seed=1)
+    
+    # Training data
     X_train = np.array([[-1.43815556, 0.10089809, -1.25432937, 1.48410426],
                         [-1.81784194, 0.42935033, -1.2806198, 0.06527391]])
-    print(model.predict(X_train))
     Y_train = np.array([[1, 0, 0, 1], [0, 1, 1, 0]])
+    
+    # Initial predictions
+    print("Initial predictions:", model.predict(X_train))
+    
+    # Initialize weights to zero and display initial weights
     model.initialize_all_weights_to_zeros()
-    print("****** Model weights ******\n",model.print_weights())
-    print("****** Input samples ******\n",X_train)
-    print("****** Desired Output ******\n",Y_train)
-    percent_error=[]
-    for k in range (20):
+    print("Initial weights:\n", model.print_weights())
+    
+    # Train the model and display percent error over epochs
+    percent_error = []
+    for k in range(20):
         model.train(X_train, Y_train, num_epochs=1, alpha=0.0001)
-        percent_error.append(model.calculate_percent_error(X_train,Y_train))
-    print("******  Percent Error ******\n",percent_error)
-    print("****** Model weights ******\n",model.weights)
+        percent_error.append(model.calculate_percent_error(X_train, Y_train))
+    
+    # Display percent error and final weights
+    print("Percent error over epochs:", percent_error)
+    print("Final weights:\n", model.print_weights())
